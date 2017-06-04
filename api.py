69d7deac0b2ee933d2a32b9237f9b883f98d1447
James Liu <james_liu@fastmail.com>
2017-06-04 15:20:57 +1000

    renamed dictionary keys for airports endpoint to match Semantic UI search component params

--- api.py
+++ api.py
@@ -1,20 +1,28 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS, cross_origin
 import json
 from flypy import *
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+
 class Airports(Resource):
     def get(self):
         with open('flypy/data/airports.json') as json_data:
             d = json.load(json_data)
-            return [{k: x[k] for k in ('name', 'iata_code', 'country')} for x in d]
+            trimmed = [{k: x[k] for k in ('name', 'iata_code', 'country')} for x in d]
+            for x in trimmed:
+                x["title"] = x.pop("name")
+                x["price"] = x.pop("iata_code")
+                x["description"] = x.pop("country")
+            return trimmed
 
 
 api.add_resource(HelloWorld, '/')
